68f02ddb84d7f099bf9b6656270368c2f497aadb
KsKab <kabeer@iitk.ac.in>
2019-06-01 01:37:39 +0530

    Added include directory

---
+++ include/helipad_det/Preprocess.h
@@ -0,0 +1,10 @@
+cv::Mat Preprocess(cv::Mat img,int canny_edgeThres, int canny_lowThres, int canny_ratio, int canny_kernel_size){
+    cv::Mat gray, blur, canny, result;
+
+    cv::cvtColor(img,gray,CV_BGR2GRAY);
+    cv::GaussianBlur(gray, blur, cv::Size(5,5), 0, 0);
+    cv::Canny(blur, canny, canny_lowThreshold, canny_lowThreshold*cany_ratio, canny_kernel_size);
+
+    cv::cvtColor(canny,result,CV_GRAY2BGR);
+    return result;
+}
