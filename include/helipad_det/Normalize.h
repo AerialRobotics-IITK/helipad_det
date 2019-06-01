f29e301362a443006716bb2c2131acd26e83f92e
Tanay <tanays@iitk.ac.in>
2019-06-01 16:11:49 +0530

    Arrays changed to vectors

--- include/helipad_det/Normalize.h
+++ include/helipad_det/Normalize.h
@@ -1,35 +1,35 @@
+#include <opencv2/opencv.hpp>
 #include <math.h>
+#include <iostream>
 
-void smooth(int* d, int size){
+void smooth(std::vector<double> input, std::vector<double>& output){
     int i;
-    int temp[size]={};
-    if(d[0]>d[1])
-        temp[0]=d[0];
-    if(d[size-1]>d[size-2])
-        temp[size-1]=d[size-1];
-    if(temp[0]!=0 && temp[size-1]!=0)
+    if(input[0]>input[1])
+        output[0]=input[0];
+    if(input[input.size()-1]>input[input.size()-2])
+        output[input.size()-1]=input[input.size()-1];
+    if(output[0]!=0 && output[input.size()-1]!=0)
     {
-        if(temp[0]>temp[size-1])
-            temp[size-1]=0;
+        if(output[0]>output[input.size()-1])
+            output[input.size()-1]=0;
         else
-            temp[0]=0;    
+            output[0]=0;    
     }
-    for(i=1;i<size-1;i++)
+    for(i=1;i<input.size()-1;i++)
     {
-        if(d[i]>d[i+1] && d[i]>d[i-1])
-            temp[i]=d[i];
+        if(input[i]>input[i+1] && input[i]>input[i-1])
+            output[i]=input[i];
         else
-            temp[i]=0;
+            output[i]=0;
     }
-    for(i=0;i<size;i++)
-        d[i]=temp[i];
 }
 
-void normalize(int* d, int n, int size){
+void normalize(std::vector<double>& input, std::vector<double>& output, int n){
     int i;
-    int temp[size];
-    for(i=0;i<size;i++)
-        temp[i]=asin((2*d[i]*sqrt(n*n-d[i]*d[i]))/(n*n));
-    for(i=0;i<size;i++)
-        d[i]=temp[i];
+    for(i=0;i<input.size();i++)
+    {
+        if(input[i]==0)
+            output[i]=3.1416;
+        else
+            output[i]=asin((2*input[i]*sqrt(n*n-input[i]*input[i]))/(n*n));
 }
