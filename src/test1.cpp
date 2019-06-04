dd3820915d8d897f2a568315cb30442f14d91378
Tanay <tanays@iitk.ac.in>
2019-06-04 13:03:04 +0530

    Optimized

--- src/test1.cpp
+++ src/test1.cpp
@@ -12,7 +12,7 @@ int main(int argc, char** argv)
     param_get.getParam("/helipad_det/low_threshold", canny_lowThres);
     param_get.getParam("/helipad_det/ratio", ratio);
     param_get.getParam("/helipad_det/kernel_size", kernel_size);
-    cv::Mat img = cv::imread("/home/kabeer/H_det/src/helipad_det/etc/Refined H.png");
+    cv::Mat img = cv::imread("/home/tanay/catkin_ws/src/helipad_det/etc/Refined H.png");
     ROS_ASSERT(img.empty()!=true);
     // cv::imshow("img", img);
     cv::Mat prepro_img = Preprocess(img, canny_lowThres, ratio, kernel_size);
@@ -20,31 +20,30 @@ int main(int argc, char** argv)
     cv::waitKey(0);
     cv::destroyAllWindows();
     std::vector<std::vector<cv::Point> > ListContours;
-
-    std::vector<cv::Vec4i> hierarchy;
-    cv::findContours(prepro_img, ListContours, hierarchy, CV_RETR_LIST, CV_CHAIN_APPROX_NONE);
-	std::vector<std::vector<double> > ListDistance(ListContours.size());
+    cv::findContours(prepro_img, ListContours, CV_RETR_LIST, CV_CHAIN_APPROX_NONE);
+	std::vector<double> Distances;
     for(i=0;i<ListContours.size();i++)
     {
+        std::vector<double> Distances;
         cv::Mat img_tmp;
         img_tmp = img.clone();
-        pointToLineDistance(ListContours.at(i), ListDistance.at(i));
-        std::vector<double> temp(ListDistance.at(i).size());
-        smooth(ListDistance.at(i), temp);
+        pointToLineDistance(ListContours.at(i), Distances);
+        std::vector<double> temp(Distances.size());
+        smooth(Distances, temp);
         graph(temp, "Refined Graph");
         if(isSimilar(temp)==1)
-        {
             std::cout << "CHAAP-ED" << std::endl;
-        }
         cv::drawContours(img_tmp, ListContours, i, cv::Scalar(255, 0, 255));
         Retrace(temp, ListContours.at(i), img_tmp);
         cv::imshow("contours", img_tmp);
         if((char)cv::waitKey(0)=='q')
             return -1;
+        Distances.clear();
         cv::destroyAllWindows();
         ros::spinOnce();
         // for(int j=0;j<temp.size();j++)
         //     std::cout << temp.at(j) << std::endl;
         // std::cout << std::endl << std::endl;
     }
+    return 0;
 }
