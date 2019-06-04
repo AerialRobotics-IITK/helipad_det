41615362ad354f30e35067704ce3f3b44875a562
KsKab <kabeer@iitk.ac.in>
2019-06-04 22:23:14 +0530

    Changes in hdetect.cpp

--- src/hdetect.cpp
+++ src/hdetect.cpp
@@ -5,7 +5,8 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <helipad_det/Preprocess.h>
-#include <helipad_det/Distance.h>
+#include <helipad_det/Signature Processing.h>
+#include <helipad_det/isSimilar.h>
 
 int lowThreshold;
 int ratio;
@@ -27,9 +28,9 @@ class ImageConverter{
 	    image_sub = it_.subscribe("usb_cam/image_raw", 1,
 	      &ImageConverter::imageCb, this);
 	    image_pub = it_.advertise("threshold_image", 1);
-			nh.getParam("/hdetect/low_threshold", lowThreshold);
-			nh.getParam("/hdetect/ratio", ratio);
-			nh.getParam("/hdetect/kernel_size", kernel_size);
+			nh.getParam("/helipad_det/low_threshold", lowThreshold);
+			nh.getParam("/helipad_det/ratio", ratio);
+			nh.getParam("/helipad_det/kernel_size", kernel_size);
 	  }
 
 	  ~ImageConverter(){}
@@ -48,7 +49,7 @@ class ImageConverter{
 
 	    img=cv_ptr->image;
 	    
-	    preprocess_result=Preprocess(img, lowThreshold, ratio, kernel_size);
+	    /*preprocess_result=Preprocess(img, lowThreshold, ratio, kernel_size);
 	    
 	    std::vector<std::vector<cv::Point> > ListContours;
 	    std::vector<std::vector<double> > ListDistance;
@@ -59,14 +60,51 @@ class ImageConverter{
 			for(unsigned int i = 0; i< ListContours.size();i++){
 					n = round(cv::arcLength(ListContours.at(i), true)/(26*3));
 					pointToLineDistance(ListContours.at(i), ListDistance[i]);
-			}
-	    
-	    cv::cvtColor(preprocess_result,result,CV_GRAY2BGR);
+			}*/
+
+	    ROS_ASSERT(img.empty()!=true);
+    	// cv::imshow("img", img);
+    	cv::Mat prepro_img = Preprocess(img, lowThreshold, ratio, kernel_size);
+
+    	
+
+    	//cv::imshow("Preprocessed Image", prepro_img);
+    	//cv::waitKey(0);
+    	//cv::destroyAllWindows();
+    	std::vector<std::vector<cv::Point> > ListContours;
+
+    	std::vector<cv::Vec4i> hierarchy;
+    	cv::findContours(prepro_img, ListContours, hierarchy, CV_RETR_LIST, CV_CHAIN_APPROX_NONE);
+		std::vector<std::vector<double> > ListDistance(ListContours.size());
+    	for(int i=0;i<ListContours.size();i++)
+    	{
+        	cv::Mat img_tmp;
+        	img_tmp = img.clone();
+        	pointToLineDistance(ListContours.at(i), ListDistance.at(i));
+        	std::vector<double> temp(ListDistance.at(i).size());
+        	smooth(ListDistance.at(i), temp);
+        	//graph(temp, "Refined Graph");
+        	if(isSimilar(temp)==1)
+        	{
+            	std::cout << "CHAAP-ED" << std::endl;
+            	cv::drawContours(img_tmp, ListContours, i, cv::Scalar(255, 0, 255));
+        		Retrace(temp, ListContours.at(i), img_tmp);
+        	}
+        cv::cvtColor(prepro_img,img_tmp,CV_GRAY2BGR);
 	    cv_bridge::CvImage Can_img;
- 			Can_img.header.stamp = ros::Time::now();
- 			Can_img.encoding = sensor_msgs::image_encodings::BGR8;
- 			Can_img.image = result;
- 			image_pub.publish(Can_img.toImageMsg());
+ 		Can_img.header.stamp = ros::Time::now();
+ 		Can_img.encoding = sensor_msgs::image_encodings::BGR8;
+ 		Can_img.image = img_tmp;
+ 		image_pub.publish(Can_img.toImageMsg());
+        //cv::imshow("contours", img_tmp);
+        //if((char)cv::waitKey(0)=='q')
+            //return -1;
+        //cv::destroyAllWindows();	
+        ros::spinOnce();
+        // for(int j=0;j<temp.size();j++)
+        //     std::cout << temp.at(j) << std::endl;
+        // std::cout << std::endl << std::endl;
+    	}
       return;
   	}
 
